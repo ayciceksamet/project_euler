06e0343fbbb2c174076ee01afd9639fe21302312
aycicek <ayciceksamet@gmail.com>
2016-10-27 15:36:19 +0300

    project euler 2 solution

--- projectEuler2/projectEuler2.cpp
+++ projectEuler2/projectEuler2.cpp
@@ -2,10 +2,33 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
 
-
+using namespace std;
 int main()
 {
+	int first = 0;
+	int second = 1;
+	int buffer;
+	int sum = 0;
+	int result;
+	do{
+		
+	    result = first + second;
+		second = first;
+		first = result;
+		cout << "degerler :" << result << endl;
+
+		if (result % 2 == 0) {
+			sum = sum + result;
+		}
+		
+	} while (result < 40);
+
+
+	cout << "toplam :" << sum << endl;
+
+	system("pause");
     return 0;
 }
 
